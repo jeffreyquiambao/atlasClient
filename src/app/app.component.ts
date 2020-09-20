738802a63a42c2d7a92f35baccbff34a760f6fb9
Jeffrey Quiambao <jcquiamb@uwaterloo.ca>
2020-09-20 17:59:59 -0400

    analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { filter } from 'rxjs/operators';
+import {Router, NavigationEnd} from '@angular/router';
+declare var gtag;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'atlasClient';
+  constructor(router: Router) {
+    const navEndEvents = router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+    );
+    navEndEvents.subscribe((event: NavigationEnd) => {
+      gtag('config', 'UA-178510197-1', {
+        'page-path': event.urlAfterRedirects
+      });
+    });
+  }
 }
